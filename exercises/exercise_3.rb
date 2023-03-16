a93ae0871f85139f84170b7df0ad7e46ca0ccb9c
Lucas Sahota <lucaswade4@gmail.com>
2023-03-16 15:40:32 -0700

    Update exercise 3 to not delete store3, complete exercise 6

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -7,6 +7,5 @@ puts "----------"
 
 # Your code goes here ...
 @store3 = Store.find_by(id: 3)
-@store3.destroy
 
 puts "#{Store.count}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Lucas", last_name: "Wade", hourly_rate: 25)
+@store1.employees.create(first_name: "Stefan", last_name: "Mogats", hourly_rate: 15)
+
+@store2.employees.create(first_name: "Steven", last_name: "Walgo", hourly_rate: 50)
+@store2.employees.create(first_name: "Sugma", last_name: "Nuts", hourly_rate: 15)
+@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 15)
+
+@store3.employees.create(first_name: "Mike", last_name: "John", hourly_rate: 45)
+@store3.employees.create(first_name: "Filip", last_name: "Jones", hourly_rate: 30)
+@store3.employees.create(first_name: "Mohammad", last_name: "Ali", hourly_rate: 20)
+
+
