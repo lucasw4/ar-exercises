5c544cbb309dc1d259a28282a364828d0c0f876c
Lucas Sahota <lucaswade4@gmail.com>
2023-03-16 15:54:53 -0700

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "enter a store name"
+@store_input = gets.chomp
+
+store = Store.create(name: @store_input)
+
+if store.save
+  puts "Store record saved successfully"
+else
+  # Display the error messages to the user
+  puts "Could not save store record due to the following errors:"
+  store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+end
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greather_than_or_equal_to: 15, less_than_or_equal_to: 200 }
+  validates :store, presence: true
 end
